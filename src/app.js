e0557cb0017aa2c84de95d2bd764afe0df857f65
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-08 13:16:04 -0600

    added route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const validateBearerToken = require('./validate-bearer-token')
 const errorHandler = require('./error-handler')
+//const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
@@ -16,7 +17,7 @@ app.use(cors())
 app.use(helmet())
 app.use(validateBearerToken)
 
-app.use(bookmarksRouter)
+//app.use(bookmarksRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
